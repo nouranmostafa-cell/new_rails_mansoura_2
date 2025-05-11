facaa5f2a285e8b53a8149f5d8e7bb1aca68871e
nouranmostafa-cell <nouranmostafa1234@gmail.com>
2025-05-11 07:34:34 +0300

    Adding Users and posts CRUD operations

---
+++ db/migrate/20250511041122_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.date :dob
+      t.string :email
+      t.string :phone_number
+
+      t.timestamps
+    end
+    
+    add_index :users, :email, unique: true
+  end
+end
